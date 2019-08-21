ae71addcaf687eccab04205f62f40c84596c4af5
Magda <magdabaj@protonmail.com>
2019-08-21 12:52:42 +0200

    Loading courses with Redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,90 +1,64 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from 'prop-types';
 import {bindActionCreators} from "redux";
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //
-    //     this.state = {
-    //         course: {
-    //             title: ""
-    //         }
-    //     };
-    //
-    //     this.handleChange = this.handleChange.bind(this);
-    // }
 
-    // handleChange(event) {
-    //     const course = { ...this.state.course, title: event.target.value};
-    //     this.setState({
-    //         course: course,
-    //     })
-    // }
+    componentDidMount() {
+        const {courses, authors, actions} = this.props;
 
-    //less code and no binding necessary
-
-    state = {
-        course: {
-            title: ""
+        if(courses.length === 0) {
+            actions.loadCourses().catch(error => {
+                alert("Loading courses failed" + error);
+            });
         }
-    };
 
-    // Another way of writing simple handleChange(event){...} function
-    handleChange = event => {
-        const course = { ...this.state.course, title: event.target.value};
-        this.setState({
-            course: course,
-        })
-    };
-
-    // Doesn't work
-    // handleSubmit(event) {
-    //     event.preventDefault();
-    //     alert(this.state.course.title);
-    // }
-
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.actions.createCourse(this.state.course);
-    };
+        if(authors.length === 0) {
+            actions.loadAuthors().catch(error => {
+                alert("Loading authors failed" + error);
+            });
+        }
+    }
 
     render() {
         return (
-            // Submit in form enable the user to submit also by enter key
-            <form onSubmit={this.handleSubmit}>
+            <>
                 <h2>Courses</h2>
-                <h3>Add Course</h3>
-                <input
-                    type={"text"}
-                    onChange={this.handleChange}
-                    value={this.state.course.title}
-                />
-                <input type={"submit"} value={"Save"}/>
-                {this.props.courses.map(course => (
-                    <div key={course.title}>{course.title}</div>
-                ))}
-            </form>
+                <CourseList courses={this.props.courses}/>
+            </>
         )
     }
 }
 
 CoursesPage.propTypes = {
+    authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
     return {
-        courses: state.courses
+        courses: state.authors.length === 0 ? []
+            : state.courses.map(course => {
+            return {
+                ...course,
+                authorName: state.authors.find(a => a.id === course.authorId).name
+            }
+        }),
+        authors: state.authors
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        actions: bindActionCreators(courseActions, dispatch)
+        actions: {
+            loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+            loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+        }
     };
 }
 
